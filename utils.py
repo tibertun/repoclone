b19870f0c1320b160d8a480a4e647ed9348ac9ea
Alicedomik <alicedomnik6@gmail.com>
2026-02-22 13:28:13 +0200

    added isPrime function

--- utils.py
+++ utils.py
@@ -5,3 +5,11 @@ def factorial(n):
     for i in range(1, n + 1):
         result *= i
     return result
+
+def isPrime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5)+1):
+        if n % i == 0:
+            return False
+    return True
